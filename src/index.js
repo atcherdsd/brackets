4847a12abadf86115c345e2804325e4714a3424a
Serg <tchernak.csg@gmail.com>
2022-02-23 17:47:00 +0300

    decode brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const matrixIndex = bracketsConfig.findIndex(config =>
+        config.includes(str[i]));
+    if (str[i] === bracketsConfig[matrixIndex][1] 
+        && stack[stack.length - 1] === bracketsConfig[matrixIndex][0])
+      stack.pop();
+
+    else stack.push(str[i]);
+    };
+  return stack.length === 0;
 }
